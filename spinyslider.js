cc019ade1dc5f7b3364efe92e06372c3603323b4
Nikhil John <nikhiljohn1010@gmail.com>
2015-11-20 02:08:51 +0530

    Revert "Dynamic slider elements"
    This reverts commit c688e4c39e2c310d23e2995221f21d676af2709a.

--- spinyslider.js
+++ spinyslider.js
@@ -1,45 +1,29 @@
-var isLoaded = false, slider,
-    images = [{
-        id: 1,
-        url: "images/opera.png"
-    }, {
-        id: 2,
-        url: "images/chrome.png"
-    }, {
-        id: 3,
-        url: "images/firefox.png"
-    }, {
-        id: 4,
-        url: "images/safari.png"
-    }, {
-        id: 5,
-        url: "images/ie.png"
-    }];
-var slideClasses = [
-    "wide-slide left-slide",
-    "side-slide left-slide",
-    "main-slide center-slide",
-    "side-slide right-slide",
-    "wide-slide right-slide"
-];
-
-function init() {
-    slider = $(".slider");
-    for (var i in images) {
-        if (images.hasOwnProperty(i)) {
-            slider.append("<span data-slide=\"" + images[i].id + "\"><img class=\"" + slideClasses[i] + "\" src=\"" + images[i].url + "\" alt=\"" + images[i].id + "\" /></span>");
-        }
+var isLoaded = false,
+    slider, img = [],
+    images = [
+        "images/opera.png",
+        "images/chrome.png",
+        "images/firefox.png",
+        "images/safari.png",
+        "images/ie.png"
+    ];
+for (var i in images) {
+    if (images.hasOwnProperty(i)) {
+        img.push("<span id=\"slide-" + (parseInt(i) + 1) + "\"><img src=\"" + images[i] + "\" /></span>");
     }
+}
+
+function init(slides) {
+    slider = $(".slider");
     isLoaded = true;
 }
 
 $(document).ready(function() {
     if (isLoaded) {
         slider.on("click", "img", function(event) {
-            var slide = $(event.target).parent();
-            var sid = slide[0].dataset.slide);
+            console.log(event);
         });
     }
 });
 
-init();
+init(img);
