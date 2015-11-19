eea016df485c17f55df270687224c20d16c50eed
Nikz Jon <nikhiljohn1010@gmail.com>
2015-11-19 18:28:59 +0530

    Files added

---
+++ spinyslider.js
@@ -0,0 +1,22 @@
+var slider,img = [],
+  images = [
+    "images/opera.png",
+    "images/chrome.png",
+    "images/firefox.png",
+    "images/safari.png",
+    "images/ie.png"
+  ];
+for (var i in images) {
+  if (images.hasOwnProperty(i)) {
+    img.push("<span id=\"slide-"+i+"\"><img src=\"" + images[i] + "\" /></span>");
+  }
+}
+
+function init(slides){
+ slider = $(".slider");
+ 
+}
+
+console.log(img[2]);
+
+init(img);
