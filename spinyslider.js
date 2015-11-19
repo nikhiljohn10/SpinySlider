b7bb8c5fd88c8232b050284543a52311d0740fcb
Nikhil John <nikhiljohn1010@gmail.com>
2015-11-20 01:20:22 +0530

    Modifiying fork link

--- spinyslider.js
+++ spinyslider.js
@@ -1,22 +1,29 @@
-var slider,img = [],
-  images = [
-    "images/opera.png",
-    "images/chrome.png",
-    "images/firefox.png",
-    "images/safari.png",
-    "images/ie.png"
-  ];
+var isLoaded = false,
+    slider, img = [],
+    images = [
+        "images/opera.png",
+        "images/chrome.png",
+        "images/firefox.png",
+        "images/safari.png",
+        "images/ie.png"
+    ];
 for (var i in images) {
-  if (images.hasOwnProperty(i)) {
-    img.push("<span id=\"slide-"+(parseInt(i)+1)+"\"><img src=\"" + images[i] + "\" /></span>");
-  }
+    if (images.hasOwnProperty(i)) {
+        img.push("<span id=\"slide-" + (parseInt(i) + 1) + "\"><img src=\"" + images[i] + "\" /></span>");
+    }
 }
 
-function init(slides){
- slider = $(".slider");
- 
+function init(slides) {
+    slider = $(".slider");
+    isLoaded = true;
 }
 
-console.log(img[0]);
+$(document).ready(function() {
+    if (isLoaded) {
+        slider.on("click", "img", function(event) {
+            console.log(event);
+        });
+    }
+});
 
 init(img);
